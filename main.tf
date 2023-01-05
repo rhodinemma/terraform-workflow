91d1c257f88b6a349a2ef439be59a287cee6dc07
rhodinemma <rhodinemma10@gmail.com>
2023-01-05 16:22:58 +0300

    ft(setup-load-balancer-listener)

--- main.tf
+++ main.tf
@@ -81,3 +81,22 @@ resource "aws_security_group_rule" "allow_http_inbound" {
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 }
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.load_balancer.arn
+
+  port = 80
+
+  protocol = "HTTP"
+
+  # By default, return a simple 404 page
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
