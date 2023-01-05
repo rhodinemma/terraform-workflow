81e15f8a886550a31c8fe5998a81c70339d21699
rhodinemma <rhodinemma10@gmail.com>
2023-01-05 16:21:21 +0300

    ft(add-security-group-and-rule)

--- main.tf
+++ main.tf
@@ -67,3 +67,17 @@ data "aws_vpc" "default_vpc" {
 data "aws_subnet_ids" "default_subnet" {
   vpc_id = data.aws_vpc.default_vpc.id
 }
+
+resource "aws_security_group" "instances" {
+  name = "instance-security-group"
+}
+
+resource "aws_security_group_rule" "allow_http_inbound" {
+  type              = "ingress"
+  security_group_id = aws_security_group.instances.id
+
+  from_port   = 8080
+  to_port     = 8080
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
