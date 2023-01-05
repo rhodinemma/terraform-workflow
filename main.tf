eb2cc721d76556eae68098ccbd68fa9589c3b2a5
rhodinemma <rhodinemma10@gmail.com>
2023-01-05 16:20:24 +0300

    ft(add-vpc-and-subnet)

--- main.tf
+++ main.tf
@@ -59,3 +59,11 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_c
     }
   }
 }
+
+data "aws_vpc" "default_vpc" {
+  default = true
+}
+
+data "aws_subnet_ids" "default_subnet" {
+  vpc_id = data.aws_vpc.default_vpc.id
+}
