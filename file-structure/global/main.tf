3ba7b8dafb6f4727abfd0304b81f46e19c6fcae8
rhodinemma <rhodinemma10@gmail.com>
2023-01-07 22:51:56 +0300

    ch(separate-environments)

---
+++ file-structure/global/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  backend "remote" {
+    organization = "rhodin"
+
+    workspaces {
+      name = "rhodin-workspace"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Route53 zone is shared across staging and production
+resource "aws_route53_zone" "primary" {
+  name = "devopsdeployed.com"
+}
---
+++ file-structure/staging/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  backend "remote" {
+    organization = "rhodin"
+
+    workspaces {
+      name = "rhodin-workspace"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "web_app" {
+  source = "../../web-app/my-module"
+
+  # Input Variables
+  bucket_name      = "rhodin-web-app-data-${local.environment_name}"
+  domain           = "devopsdeployed.com"
+  environment_name = local.environment_name
+  instance_type    = "t2.micro"
+  create_dns_zone  = false
+  db_name          = "${local.environment_name}mydb"
+  db_user          = "foo"
+  db_pass          = "foobas"
+}
