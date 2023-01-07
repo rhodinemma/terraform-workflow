dbcf5145074a09ca77d505144f09ff00fd32744d
rhodinemma <rhodinemma10@gmail.com>
2023-01-08 02:44:51 +0300

    ch(update-rds)

--- web-app/my-module/database.tf
+++ web-app/my-module/database.tf
@@ -1,11 +1,11 @@
 resource "aws_db_instance" "db_instance" {
-  allocated_storage   = 20
-  storage_type        = "standard"
+  identifier          = var.db_name
+  instance_class      = "db.t3.micro"
+  allocated_storage   = 10
   engine              = "postgres"
-  engine_version      = "12.5"
-  instance_class      = "db.t2.micro"
-  name                = var.db_name
+  engine_version      = "13.7"
   username            = var.db_user
   password            = var.db_pass
+  publicly_accessible = true
   skip_final_snapshot = true
 }
