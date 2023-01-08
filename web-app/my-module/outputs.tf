9b158269546cfa5c5e1dbc9595dbd238c50c9a65
rhodinemma <rhodinemma10@gmail.com>
2023-01-08 03:18:04 +0300

    ch(last-fix)

--- web-app/my-module/outputs.tf
+++ web-app/my-module/outputs.tf
@@ -5,7 +5,3 @@ output "instance_1_ip_addr" {
 output "instance_2_ip_addr" {
   value = aws_instance.instance_2.public_ip
 }
-
-output "db_instance_addr" {
-  value = aws_db_instance.db_instance.address
-}
